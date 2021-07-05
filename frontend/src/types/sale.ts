8a0ac2543993a0bb18497dd154bb75a44bd928bd
Diego Alessandro <diego_alessandro@hotmail.com>
2021-07-05 13:08:30 -0300

    BarChart Integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,12 @@
+//tipos customizados do projeto
+
 export type SaleSum = {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
+}
+
+export type SaleSuccess= {
+    sellerName : string;
+    visited: number;
+    deals: number;
 }
