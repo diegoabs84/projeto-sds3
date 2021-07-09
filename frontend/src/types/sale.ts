d329e88a6e50d900d2fc508ca67465bb419a9bd5
Diego Alessandro <diego_alessandro@hotmail.com>
2021-07-09 09:58:02 -0300

    Datatable Integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,27 @@
+import { Seller } from "./seller"
+
 //tipos customizados do projeto
+export type Sale = {
+    visited: number,
+    deals: number,
+    amount:number,
+    date: string,
+    seller: Seller 
+    id: number
+}
+        
+export type SalePage = {
+    content? : Sale[];
+    last: boolean,
+    totalElements: number,
+    totalPages: number,
+    size?: number,
+    number: number,
+    first: boolean,
+    numberOfElements?: number,
+    empty?: boolean
+}
+
 
 export type SaleSum = {
     sellerName: string;
